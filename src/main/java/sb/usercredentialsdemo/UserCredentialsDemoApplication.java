01240927b993f52905e12db56a44006dc5cd9521
Stephen <stephenbrady365@gmail.com>
2021-07-19 21:28:11 +0100

    Add spring and dependencies

---
+++ src/main/java/sb/usercredentialsdemo/UserCredentialsDemoApplication.java
@@ -0,0 +1,13 @@
+package sb.usercredentialsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCredentialsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserCredentialsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sb/usercredentialsdemo/UserCredentialsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package sb.usercredentialsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserCredentialsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
