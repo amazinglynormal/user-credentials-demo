b9a78c394dd1b57ab7a6fccf80a0160ee1cd85fc
Stephen <stephenbrady365@gmail.com>
2021-07-20 21:13:01 +0100

    Refactor User model to include lombok constructors

--- src/main/java/sb/usercredentialsdemo/models/User.java
+++ src/main/java/sb/usercredentialsdemo/models/User.java
@@ -1,6 +1,9 @@
 package sb.usercredentialsdemo.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,12 +12,17 @@ import javax.persistence.Id;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
     private String email;
+
     private String username;
+
     private String password;
 }
