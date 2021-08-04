a8671de7d626cc43bacb4f6f01ed1c9c4f9d3ddf
Stephen <stephenbrady365@gmail.com>
2021-08-04 20:28:12 +0100

    Add React Router and switch to App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,24 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import { Navbar } from "./components/Navbar";
+import { Container } from "./components/Container";
+import { SignUp } from "./components/SignUp";
+import { LogIn } from "./components/LogIn";
+
 export const App = () => {
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
+    <Router>
+      <Navbar />
+      <Container>
+        <Switch>
+          <Route path="/signup">
+            <SignUp />
+          </Route>
+          <Route exact path={["/", "/login"]}>
+            <LogIn />
+          </Route>
+        </Switch>
+      </Container>
+    </Router>
   );
 };
