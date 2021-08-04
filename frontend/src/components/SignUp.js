640ca3b1c8a92af2b252acb520db5a048a34476c
Stephen <stephenbrady365@gmail.com>
2021-08-04 20:01:00 +0100

    Add full Sign Up page layout

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -1,24 +1,32 @@
 import { Panel } from "./Panel";
 import { FormInput } from "./FormInput";
+import { FormButton } from "./FormButton";
 import styles from "./SignUp.module.css";
 
 export const SignUp = () => {
   const onFormSubmit = (e) => {
     e.preventDefault();
+    console.log("form submitted");
   };
 
   return (
     <div>
-      <h1>Create an account</h1>
-      <p>It's easy and free.</p>
+      <h1 className={styles.header}>Create an account</h1>
+      <p className={styles.subHeader}>It's easy and free.</p>
       <Panel>
         <form onSubmit={onFormSubmit}>
           <FormInput label="Username" id="username" />
           <FormInput type="email" label="Email" id="email" />
           <FormInput type="password" label="Password" id="password" />
-          <button type="submit">Create account</button>
+          <FormButton text="Create account" />
         </form>
       </Panel>
+      <p className={styles.logInOption}>
+        Already have an account?{" "}
+        <a href="/" className={styles.logInLink}>
+          Log in here
+        </a>
+      </p>
     </div>
   );
 };
