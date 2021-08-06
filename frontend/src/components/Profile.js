c92e140926941e9a3eba794d1b9e3a0c66e67900
Stephen <stephenbrady365@gmail.com>
2021-08-06 10:41:39 +0100

    Refactor Profile component

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,6 +1,6 @@
-import { ChangeDetailForm } from "./ChangeDetailForm";
+import { ProfileSection } from "./ProfileSection";
 import { CountrySelect } from "./CountrySelect";
-import { DetailDisplay } from "./DetailDisplay";
+import { AccountDetails } from "./AccountDetails";
 import { DeleteAccount } from "./DeleteAccount";
 
 import styles from "./Profile.module.css";
@@ -9,34 +9,15 @@ export const Profile = () => {
   return (
     <div>
       <h1>Profile</h1>
-      <div>
-        <h2 className={styles.h2Header}>
-          What country would you like to visit next?
-        </h2>
+      <ProfileSection header="What country would you like to visit next?">
         <CountrySelect />
-      </div>
-      <div>
-        <h2 className={styles.h2Header}>Details</h2>
-        <div>
-          <DetailDisplay name="Username" data="Test User" />
-        </div>
-        <div>
-          <DetailDisplay name="Email" data="Test User" />
-        </div>
-        <div>
-          <DetailDisplay name="Password" data="Test User" />
-          <ChangeDetailForm
-            id="username"
-            type="text"
-            label="New Username"
-            autocomplete="username"
-          />
-        </div>
-      </div>
-      <div>
-        <h2 className={styles.h2Header}>Delete account</h2>
+      </ProfileSection>
+      <ProfileSection header="Details">
+        <AccountDetails />
+      </ProfileSection>
+      <ProfileSection header="Delete Account">
         <DeleteAccount />
-      </div>
+      </ProfileSection>
     </div>
   );
 };
